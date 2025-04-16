ae08271f38504ef0d357e1edafb16a4ec413047c
RahaviSiri <rahavi24siri@gmail.com>
2025-04-16 11:50:23 +0530

    Removed client router,controller and added it to user

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getUserById, getUserData, loginUser, registerUser } from "../controllers/userController.js";
+import { getUserById, getUserData, loginUser, registerUser,registerCoach } from "../controllers/userController.js";
 import authUser from "../middleware/authUser.js";
 
 const userRouter = express.Router();
@@ -8,5 +8,6 @@ userRouter.post("/register",registerUser);
 userRouter.post("/login",loginUser);
 userRouter.get("/get-user-data",authUser,getUserData);
 userRouter.get("/get-user-byId/:id",getUserById);
+userRouter.get("/book-coach/:id",authUser,registerCoach);
 
 export default userRouter;
