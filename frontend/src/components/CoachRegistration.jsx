89f418c769e2e7c7d9261416a12dca2d7c63d32a
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-04-18 08:52:34 +0530

    add tailwind property

--- frontend/src/components/CoachRegistration.jsx
+++ frontend/src/components/CoachRegistration.jsx
@@ -4,6 +4,7 @@ import { CoachContext } from "../context/Coachcontext";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useState } from "react";
+import assets from "../assets/assets.js";
 
 const CoachRegistration = () => {
   const navigate = useNavigate();
@@ -49,26 +50,19 @@ const CoachRegistration = () => {
     setQualifications,
     qualifications_photo,
     setQualifications_photo,
-
-    // profileState,
-    // setProfileState,
-    // NIC_photoState,
-    // setNIC_photoState,
-    // qualificaions_photoState,
-    // setQualificaions_photoState
   } = useContext(CoachContext);
   const districts = [
     "Ampara", "Anuradhapura", "Badulla", "Batticaloa",
     "Colombo", "Galle", "Gampaha", "Hambantota",
     "Jaffna", "Kalutara", "Kandy", "Kegalle",
     "Kilinochchi", "Kurunegala", "Mannar", "Matale",
-    "Matara", "Monaragala", "Mullaitivu", "Nuwara Eliya",
+    "Matara", "Monaragala", "Mullaitivu", "Nuwara Eliya",
     "Polonnaruwa", "Puttalam", "Ratnapura", "Trincomalee",
     "Vavuniya"
   ];
 
-  const [NIC_photo_name,setNIC_photoName] = useState("");
-    const [qualifications_photo_name,setQualifications_photo_name] = useState("");
+  const [NIC_photo_name, setNIC_photoName] = useState("");
+  const [qualifications_photo_name, setQualifications_photo_name] = useState("");
 
   const fileToBase64 = (file) =>
     new Promise((resolve, reject) => {
@@ -89,25 +83,23 @@ const CoachRegistration = () => {
 
     if (savedProfile) {
       setProfile(savedProfile);
-
     }
     if (savedNIC) {
       setNIC_photo(savedNIC);
       setNIC_photoName(savedNICName)
     }
-    if (savedQual)
-      setQualifications_photo( savedQual);
-    setQualifications_photo_name(savedQualName);
+    if (savedQual) {
+      setQualifications_photo(savedQual);
+      setQualifications_photo_name(savedQualName);
+    }
   }, []);
 
   useEffect(() => {
     if (profile instanceof window.File) {
-      fileToBase64(profile).then((base64) =>{
+      fileToBase64(profile).then((base64) => {
         localStorage.setItem("coachProfile", base64)
         localStorage.setItem("coachProfile_name", profile.name);
-      }
-        
-      );
+      });
     } else if (profile) {
       localStorage.setItem("coachProfile", profile);
       localStorage.setItem("coachProfile_name", profile.name);
@@ -122,7 +114,6 @@ const CoachRegistration = () => {
       });
     } else if (NIC_photo) {
       localStorage.setItem("coachNIC", NIC_photo);
-      // localStorage.setItem("NIC_name", NIC_photo.name);
     }
   }, [NIC_photo]);
 
@@ -138,13 +129,6 @@ const CoachRegistration = () => {
     }
   }, [qualifications_photo]);
 
- console.log(qualifications_photo_name)
-
-  const imagePrevent = (e) => {
-    try {
-      e.preventDefault();
-    } catch (error) {}
-  };
   const handleSubmit = (e) => {
     try {
       e.preventDefault();
@@ -153,20 +137,26 @@ const CoachRegistration = () => {
       toast.error(error.message);
     }
   };
+  
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-50 px-4 font-sans">
-      <div className="w-full max-w-4xl bg-white p-8 rounded-2xl shadow-lg space-y-6">
+    <div
+      className="flex justify-center items-center min-h-screen bg-gray-50 px-4 font-sans"
+      style={{
+        backgroundImage: `url(${assets.coach2})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="w-full max-w-3xl bg-black/30 p-8 rounded-2xl shadow-xl space-y-6">
         <form onSubmit={handleSubmit}>
-          <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">
+          <h1 className="text-2xl font-bold text-center text-white mb-4">
             Register to Become a Coach
           </h1>
-          <p className="text-sm text-gray-600 text-center mb-6">
-            Please note: Fields marked with an asterisk (*) are required.
-          </p>
 
           {/* Personal Info */}
-          <div className="space-y-4 border border-gray-400 m-2 p-4">
-            <p className="text-lg font-semibold text-gray-800 border-b pb-1">
+          <div className="space-y-4">
+            <p className="text-lg font-semibold text-white border-b border-blue-300 pb-1">
               Personal Info:
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -175,23 +165,27 @@ const CoachRegistration = () => {
                 value={fullName}
                 type="text"
                 placeholder="Full Name*"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
                 required
               />
+              
               {profile ? (
-                <div onClick={() => setProfile(null)}>
+                <div 
+                  onClick={() => setProfile(null)}
+                  className="cursor-pointer"
+                >
                   <img
                     src={
                       profile instanceof window.File
                         ? URL.createObjectURL(profile)
                         : profile
                     }
-                    className="flex w-16 h-16 items-center gap-2 text-blue-600 hover:underline cursor-pointer text-sm font-medium border border-dashed border-blue-400 p-2 rounded-md"
-                    alt=""
+                    className="h-32 w-32 object-cover border border-blue-300 rounded-md"
+                    alt="Profile Preview"
                   />
                 </div>
               ) : (
-                <label className="flex items-center gap-2 text-blue-600 hover:underline cursor-pointer text-sm font-medium border border-dashed border-blue-400 p-2 rounded-md">
+                <label className="flex items-center gap-2 text-white hover:underline cursor-pointer text-sm font-medium border border-dashed border-white-400 p-2 rounded-md">
                   <UploadCloud size={16} /> Upload profile photo*
                   <input
                     type="file"
@@ -213,13 +207,12 @@ const CoachRegistration = () => {
                 value={DOB}
                 type="date"
                 placeholder="Date of Birth*"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
               />
-              <div className="flex items-center gap-4">
-                <span className="text-sm text-gray-700" required>
-                  Gender*:
-                </span>
-                <label className="inline-flex items-center space-x-2 text-gray-700">
+              
+              <div className="flex items-center gap-4 text-white">
+                <span className="text-sm">Gender*:</span>
+                <label className="inline-flex items-center space-x-2">
                   <input
                     value="Male"
                     checked={gender === "Male"}
@@ -230,7 +223,7 @@ const CoachRegistration = () => {
                   />
                   <span>Male</span>
                 </label>
-                <label className="inline-flex items-center space-x-2 text-gray-700">
+                <label className="inline-flex items-center space-x-2">
                   <input
                     value="Female"
                     checked={gender === "Female"}
@@ -249,18 +242,18 @@ const CoachRegistration = () => {
                 value={NIC}
                 required
                 placeholder="NIC No*"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
               />
+              
               {NIC_photo ? (
                 <div
                   onClick={() => setNIC_photo(null)}
-                  className="flex items-center gap-2 border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer transition"
+                  className="flex items-center gap-2 text-white hover:underline cursor-pointer text-sm font-medium border border-dashed border-white-400 p-2 rounded-md"
                 >
-                  <Upload size={20} className="text-blue-500" />
-                  <span>{NIC_photo_name}</span>
+                  <File size={16} /> {NIC_photo_name}
                 </div>
               ) : (
-                <label className="flex items-center gap-2 text-blue-600 hover:underline cursor-pointer text-sm font-medium border border-dashed border-blue-400 p-2 rounded-md">
+                <label className="flex items-center gap-2 text-white hover:underline cursor-pointer text-sm font-medium border border-dashed border-white-400 p-2 rounded-md">
                   <UploadCloud size={16} /> Upload NIC frontpage*
                   <input
                     required
@@ -279,8 +272,8 @@ const CoachRegistration = () => {
           </div>
 
           {/* Contact Details */}
-          <div className="space-y-4 border border-gray-400 m-2 p-4">
-            <p className="text-lg font-semibold text-gray-800 border-b pb-1">
+          <div className="space-y-4 mt-6">
+            <p className="text-lg font-semibold text-white border-b border-blue-300 pb-1">
               Contact Details:
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -289,7 +282,7 @@ const CoachRegistration = () => {
                 onChange={(e) => setContactNo(e.target.value)}
                 value={contactNo}
                 placeholder="Contact No*"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
                 required
               />
               <input
@@ -297,14 +290,14 @@ const CoachRegistration = () => {
                 onChange={(e) => setHomeTP(e.target.value)}
                 value={HomeTP}
                 placeholder="Home Telephone No"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
               />
               <input
                 type="tel"
                 onChange={(e) => setWhatsapp(e.target.value)}
                 value={whatsapp}
                 placeholder="WhatsApp No*"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
                 required
               />
               <input
@@ -312,15 +305,15 @@ const CoachRegistration = () => {
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
                 placeholder="Email Address*"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
                 required
               />
             </div>
           </div>
 
           {/* Address */}
-          <div className="space-y-4 border border-gray-400 m-2 p-4">
-            <p className="text-lg font-semibold text-gray-800 border-b pb-1">
+          <div className="space-y-4 mt-6">
+            <p className="text-lg font-semibold text-white border-b border-blue-300 pb-1">
               Address:
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -329,7 +322,7 @@ const CoachRegistration = () => {
                 onChange={(e) => setLine1(e.target.value)}
                 value={Line1}
                 placeholder="Line 1*"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
                 required
               />
               <input
@@ -337,53 +330,47 @@ const CoachRegistration = () => {
                 onChange={(e) => setLine2(e.target.value)}
                 value={Line2}
                 placeholder="Line 2"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
               />
               <input
                 type="text"
                 onChange={(e) => setCity(e.target.value)}
                 value={city}
                 placeholder="City*"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
                 required
               />
               
-
-
               <select 
                 value={district}
                 onChange={(e) => setDistrict(e.target.value)}
                 required
-                className="border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
               >
-              {/* Placeholder option */}
-              <option value="" disabled>
-                Select District*
-              </option>
-
-              {/* Map over districts array to generate options */}
-              {districts.map((d) => (
-              <option key={d} value={d}>
-                {d}
-              </option>
-              ))}
+                <option value="" disabled>
+                  Select District*
+                </option>
+                {districts.map((d) => (
+                  <option key={d} value={d}>
+                    {d}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
 
           {/* Coach Selection */}
-          <div className="space-y-4 border border-gray-400 m-2 p-4">
-            <p className="text-lg font-semibold text-gray-800 border-b pb-1">
+          <div className="space-y-4 mt-6">
+            <p className="text-lg font-semibold text-white border-b border-blue-300 pb-1">
               Coach Selection Details:
             </p>
 
-            <div className="text-sm text-gray-700 mb-2"></div>
-            <div className="flex flex-wrap gap-4">
-              Select type* :
+            <div className="flex flex-wrap gap-4 text-white">
+              <span>Select type*:</span>
               {["School", "Academics", "Personal", "Any"].map((type) => (
                 <label
                   key={type}
-                  className="inline-flex items-center space-x-2 text-gray-700"
+                  className="inline-flex items-center space-x-2"
                 >
                   <input
                     value={type}
@@ -399,16 +386,16 @@ const CoachRegistration = () => {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <input
+              <input
                 type="text"
                 onChange={(e) => setSalary(e.target.value)}
                 value={salary}
                 placeholder="Expected salary*"
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
                 required
               />
               <select
-                className="input-style border border-gray-300 p-2 rounded-md"
+                className="w-full border border-blue-300 p-2 rounded-md outline-none"
                 onChange={(e) => setSport(e.target.value)}
                 value={sport}
               >
@@ -426,19 +413,19 @@ const CoachRegistration = () => {
               onChange={(e) => setQualifications(e.target.value)}
               value={qualifications}
               placeholder="Academic Qualifications"
-              className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent transition"
+              className="w-full border border-blue-300 p-2 rounded-md outline-none"
+              rows={4}
             ></textarea>
 
             {qualifications_photo ? (
               <div
                 onClick={() => setQualifications_photo(null)}
-                className="flex items-center gap-2 border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer transition"
+                className="flex items-center gap-2 text-white hover:underline cursor-pointer text-sm font-medium border border-dashed border-white-400 p-2 rounded-md"
               >
-                <File size={16} className="text-blue-500" />
-                <span>{qualifications_photo_name}</span>
+                <File size={16} /> {qualifications_photo_name}
               </div>
             ) : (
-              <label className="flex items-center gap-2 text-blue-600 hover:underline cursor-pointer text-sm font-medium border border-dashed border-blue-400 p-2 rounded-md">
+              <label className="flex items-center gap-2 text-white hover:underline cursor-pointer text-sm font-medium border border-dashed border-white-400 p-2 rounded-md">
                 <UploadCloud size={16} /> Upload qualifications evidence*
                 <input
                   type="file"
@@ -456,7 +443,7 @@ const CoachRegistration = () => {
 
           <button
             type="submit"
-            className="w-full py-3 bg-blue-600 hover:bg-blue-700 text-white text-lg font-semibold rounded-xl transition"
+            className="mt-4 w-full py-3 bg-blue-500 hover:bg-blue-700 text-white text-lg font-semibold rounded-xl transition"
           >
             Next
           </button>
