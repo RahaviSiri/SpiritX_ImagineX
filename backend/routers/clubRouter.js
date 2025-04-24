eac29df2f6390f4627a9439f48b894ebcc45f88a
RahaviSiri <rahavi24siri@gmail.com>
2025-04-24 08:57:01 +0530

    Modify router,controllers of club and competiotion by using existing multer

--- backend/routers/clubRouter.js
+++ backend/routers/clubRouter.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import { addClub, getClubs, uploadClubImage } from '../controllers/clubController.js';
-import { v2 as cloudinary } from 'cloudinary';
+import { addClub, getClubs } from '../controllers/clubController.js';
+import upload from '../middleware/multer.js';
 
 const router = express.Router();
 
 // POST route to add a new club with image upload
-router.post('/addClub', uploadClubImage, addClub);
-
+router.post('/addClub', upload.single('img'), addClub);
 // GET route to get all clubs
 router.get('/getClub', getClubs);
 
